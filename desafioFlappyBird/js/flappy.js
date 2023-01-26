799983b161fb69bb557dd8a0df42af0425af1ccc
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-01-26 18:13:12 -0300

    Projeto completo

--- desafioFlappyBird/js/flappy.js
+++ desafioFlappyBird/js/flappy.js
@@ -117,6 +117,29 @@ function Progresso() {
   this.atualizarPontos(0);
 }
 
+function estaoSobrepostos(elementoA, elementoB) {
+  const a = elementoA.getBoundingClientRect();
+  const b = elementoB.getBoundingClientRect();
+
+  const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+  const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+  return horizontal && vertical;
+}
+
+function colidiu(passaro, barreiras) {
+  let colidiu = false;
+  barreiras.pares.forEach((ParDeBarreiras) => {
+    if (!colidiu) {
+      const superior = ParDeBarreiras.superior.elemento;
+      const inferior = ParDeBarreiras.inferior.elemento;
+      colidiu =
+        estaoSobrepostos(passaro.elemento, superior) ||
+        estaoSobrepostos(passaro.elemento, inferior);
+    }
+  });
+  return colidiu;
+}
+
 function FlappyBird() {
   let pontos = 0;
 
@@ -140,6 +163,10 @@ function FlappyBird() {
     const temporizador = setInterval(() => {
       barreiras.animar();
       passaro.animar();
+
+      if (colidiu(passaro, barreiras)) {
+        clearInterval(temporizador);
+      }
     }, 20);
   };
 }
